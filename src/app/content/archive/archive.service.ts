3b1540dcc7fa88522a506aef372b560b1d6783b8
zophrox <zophrox@gmail.com>
2023-03-21 16:33:44 +0200

    make fixes spaces between words

--- src/app/content/archive/archive.service.ts
+++ src/app/content/archive/archive.service.ts
@@ -8,31 +8,31 @@ import { LaureatInfo } from 'src/app/shared/laureatInfo.model';
 export class ArchiveService {
   constructor() {}
   competitions: CompetitionsInform[] = [
-    new CompetitionsInform(
-      'Зимовий конкурс 2022',
-      'RfQm0j6gzHQ',[    new LaureatInfo(
+    new CompetitionsInform('Зимовий конкурс 2022', 'RfQm0j6gzHQ', [
+      new LaureatInfo(
         'Вася',
         'середня',
         'orchestra_logo.png',
         'Вася був парубок моторний',
         4
-      ),    new LaureatInfo(
+      ),
+      new LaureatInfo(
         'Вася',
         'середня',
         'orchestra_logo.png',
         'Вася був парубок моторний',
         4
-      )]
-  
-
-    ),
+      ),
+    ]),
     new CompetitionsInform('Якийсь там конкурс', 'RfQm0j6gzHQ'),
     new CompetitionsInform('Якийсь там конкурс', 'RfQm0j6gzHQ'),
   ];
+
   getCompetitionsInform() {
     return this.competitions.slice();
   }
-  getCompetitionInform(index:number){
-    return this.competitions[index]
+
+  getCompetitionInform(index: number) {
+    return this.competitions[index];
   }
 }
