48e9533b756949626f33e86f3ba08624a8323067
zophrox <zophrox@gmail.com>
2023-04-04 18:25:08 +0300

    add styles for header after scroll

--- src/app/content/home/home.component.ts
+++ src/app/content/home/home.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const scrollOffset =
+      window.pageYOffset ||
+      document.documentElement.scrollTop ||
+      document.body.scrollTop ||
+      0;
 
+    if (scrollOffset >= 120) {
+      document.querySelectorAll('.header').forEach((c) => {
+        c.classList.add('scroll-style');
+        c.classList.remove('transparent-header')
+
+      });
+    } else {
+      document.querySelectorAll('.header').forEach((c) => {
+        c.classList.add('transparent-header')
+        c.classList.remove('scroll-style')
+        
+      });
+    }
+  }
 }
