1cbed9153214bdac0a8bf925839569a3f9cc3007
zophrox <zophrox@gmail.com>
2023-05-15 15:24:49 +0300

    improve header-component

--- src/app/content/home/home.component.ts
+++ src/app/content/home/home.component.ts
@@ -1,31 +1,8 @@
-import { Component, HostListener } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
-  @HostListener('window:scroll', [])
-  onWindowScroll() {
-    const scrollOffset =
-      window.pageYOffset ||
-      document.documentElement.scrollTop ||
-      document.body.scrollTop ||
-      0;
-
-    if (scrollOffset >= 120) {
-      document.querySelectorAll('.header').forEach((c) => {
-        c.classList.add('scroll-style');
-        c.classList.remove('transparent-header')
-
-      });
-    } else {
-      document.querySelectorAll('.header').forEach((c) => {
-        c.classList.add('transparent-header')
-        c.classList.remove('scroll-style')
-        
-      });
-    }
-  }
-}
+export class HomeComponent {}
