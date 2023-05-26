e288e427b5fe0920d4c7d20a724418f9bf930df7
zophrox <zophrox@gmail.com>
2023-05-26 16:39:23 +0300

    add teams photos

--- src/app/content/team/info-team.service.ts
+++ src/app/content/team/info-team.service.ts
@@ -10,9 +10,19 @@ import { TeamInform } from 'src/app/shared/team-inform.model';
 })
 export class InfoTeamService {
   aboutCoach:TeamInform[]=[
-   new TeamInform('Кунтий Олег','Диригент НАОНІ    asdfasdfasdfasdfasdf asdfasdf asdfasdfas ;lkj;lkj;lk ;lkj;lk j;lkj;lkj;lkj ;l kj;lk kj kjk ;lkjk l;lkj ','assets/team/Oleh_Kuntyy.JPG'),
-   new TeamInform('Кунтий Олег','Диригент НАОНІ','assets/team/Oleh_Kuntyy.JPG'),
-   new TeamInform('Кунтий Олег','Диригент НАОНІ','assets/team/Oleh_Kuntyy.JPG'),
+   new TeamInform('Кунтий Олег','Диригент НАОНІ    asdfasdfasdfasdfasdf asdfasdf asdfasdfas ;lkj;lkj;lk ;lkj;lk j;lkj;lkj;lkj ;l kj;lk kj kjk ;lkjk l;lkj ','assets/team/Kuntyy.jpg'),
+   new TeamInform('Олесь Журавчак','Диригент НАОНІ','assets/team/juravchak.jpg'),
+   new TeamInform('Олександр Жищенко','Диригент НАОНІ','assets/team/gyschenco.jpg'),
+   new TeamInform('Тарас Столяр','Диригент НАОНІ','assets/team/stolar.jpg'),
+   new TeamInform('Ігор Брухаль','Диригент НАОНІ','assets/team/bruchal.jpg'),   
+   new TeamInform('Любомир Матейко','Диригент НАОНІ','assets/team/mateyko.jpg'),
+   new TeamInform('Назар Павленко','Диригент НАОНІ','assets/team/pavlenko.jpg'),
+   new TeamInform('Олег Микитюк','Диригент НАОНІ','assets/team/mykytuk.jpg'),
+   new TeamInform('Денис Савельєв','Диригент НАОНІ','assets/team/savelev.jpg'),
+   new TeamInform('Нікіта Рубченко','Диригент НАОНІ','assets/team/rubchenko.jpg'),
+   new TeamInform('Петро Січко','Диригент НАОНІ','assets/team/sichko.jpg'),
+   new TeamInform('Андій Бобрик','Диригент НАОНІ','assets/team/bobryk.jpg'),
+
     
   ]
   getInform(){
